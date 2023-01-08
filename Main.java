da7756ef54b5dc86786582c2385f972a9852b164
KoYe2021 <86510819+KoYe2021@users.noreply.github.com>
2023-01-08 11:12:12 +0630

    this is written from vscode web

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("how are you?");
+    System.out.println("this is written from vscode.");
   }
 }
